5ce52b0e69afb0faf70b77937985709b7c3e3d01
antrad <antonio@nextgenerationteam.com>
2018-03-01 12:40:20 +0200

    First Commit

---
+++ Consul_POC/aspnet1/Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Consul;
+
+namespace aspnet1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            this.RegisterConsul();
+            services.AddMvc();
+        }
+
+        public WriteResult RegisterConsul()
+        {
+			var client = new ConsulClient();
+
+			var name = Assembly.GetEntryAssembly().GetName().Name;
+			var port = 5001;
+			var id = $"{name}:{port}";
+
+			var tcpCheck = new AgentServiceCheck()
+			{
+				DeregisterCriticalServiceAfter = TimeSpan.FromMinutes(1),
+				Interval = TimeSpan.FromSeconds(30),
+				TCP = $"127.0.0.1:{port}"
+			};
+
+			var httpCheck = new AgentServiceCheck()
+			{
+				DeregisterCriticalServiceAfter = TimeSpan.FromMinutes(1),
+				Interval = TimeSpan.FromSeconds(30),
+				HTTP = $"http://127.0.0.1:{port}/Ping"
+			};
+
+			var registration = new AgentServiceRegistration()
+			{
+				Checks = new[] { tcpCheck, httpCheck },
+				Address = "127.0.0.1",
+				ID = id,
+				Name = name,
+				Port = port
+			};
+
+			return client.Agent.ServiceRegister(registration).GetAwaiter().GetResult();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Consul_POC/client_consul1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Consul;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace client_consul1
+{
+    class Program
+    {
+		
+
+        static void Main(string[] args)
+        {
+            CheckServices().Wait();
+            System.Console.Write("OK");
+        }
+
+        public static async Task CheckServices()
+        {
+			var _consulClient = new ConsulClient(c =>
+			{
+				var uri = new Uri("http://127.0.0.1:8500");
+				c.Address = uri;
+			});
+
+            try
+            {
+                var services = await _consulClient.Agent.Services();
+				foreach (var service in services.Response)
+				{
+                    System.Console.WriteLine(service.Value.Service);
+                    System.Console.WriteLine(service.Value.ID);
+                    System.Console.WriteLine(service.Value.Address);
+				}
+            }
+            catch (Exception ex)
+            {
+                System.Console.Write(ex.Message);
+            }
+
+        }
+        /*
+		private static async Task CheckServerHealth()
+		{
+			var _consulClient = new ConsulClient(c =>
+			{
+				var uri = new Uri("http://127.0.0.1:8500");
+				c.Address = uri;
+			});
+			var checks = await _consulClient.Health.Service("aspnet1");
+			foreach (var entry in checks.Response)
+			{
+				var check = entry.Checks.SingleOrDefault(c => c.ServiceName == "aspnet1");
+				if (check == null) continue;
+				var isPassing = check.Status == HealthStatus.Passing;
+				var serviceUri = new Uri($"{entry.Service.Address}:{entry.Service.Port}");
+				if (isPassing)
+				{
+                    //Add to service list
+                    System.Console.WriteLine("Service local online.");
+				}
+				else
+				{
+					//Add to service list
+					System.Console.WriteLine("Service local offline.");
+				}
+			}
+		}
+		*/
+    }
+}
